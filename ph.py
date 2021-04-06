3d05d8622d0ec456e86972dbd0648f387a3e23fd
owen wijaya <owenwijaya89@gmail.com>
2021-04-06 14:19:42 +0700

    Modified data.json

--- ph.py
+++ ph.py
@@ -12,10 +12,13 @@ def collect(data):
         new_link = input('Insert new links: ').lower()
         if new_link == 'q' or new_link == 'b' or len(new_link) == 0:
             break
+        if new_link == 'd':
+            delete_link(data)
         if new_link in data:
             print('Duplicate links')
             continue
-        new_links.append(new_link)
+        else:
+            new_links.append(new_link)
     return new_links
 
 def write_json(data, new_links, filename='data.json'):
@@ -26,6 +29,14 @@ def collect_links():
     data = read_json()
     new_links = collect(data)
     write_json(data,new_links)
+
+def delete_link(data):
+    while True:
+        links_to_delete = input('Links to delete: ').lower()
+        if links_to_delete == 'b' or links_to_delete == 'q' or len(links_to_delete) == 0:
+            break
+        else:
+            data.remove(links_to_delete)
 
 
 if __name__ == '__main__':
