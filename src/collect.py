5ee05580ba757b611954b9b2132cf61638bdab14
owen wijaya <owenwijaya89@gmail.com>
2021-05-13 00:29:54 +0700

    finished the collect.py

--- src/collect.py
+++ src/collect.py
@@ -2,40 +2,43 @@ import json
 import requests
 import re
 import logging
-logging.basicConfig(level=logging.INFO, format='%(levelname)s: %(lineno)s: %(message)s')
-def read_json(filename='./data/data.json'):
-    with open(filename, 'r+', encoding='utf-8') as file:
-        data = json.load(file)['links']
-    return data
 
-def collect(data):
-    new_links = []
+from downloader import download
+logging.basicConfig(level=logging.INFO, format='%(levelname)s: %(lineno)s: %(message)s')
+def get_storage_links(filename='./data/data.json'):
+    with open(filename, 'r+', encoding='utf-8') as file:
+        data = json.load(file)
+        downloaded_data = data['downloaded_links']
+        new_data = data['new_links']
+    return downloaded_data, new_data
+
+def check(downloaded_data, new_data):
     while True:
         new_link = input('Insert new links: ').lower()
         if new_link == 'q' or new_link == 'b' or len(new_link) == 0:
             logging.info('Program has ended')
             break
-        if new_link in data:
+        if new_link in downloaded_data:
             logging.warning('Duplicate links in storage')
             continue
-        if new_link in new_links:
+        if new_link in new_data:
             logging.warning('Duplicate links in recent entered link')
             continue
         if 'https://www.pornhub.com/view_video.php?viewkey' not in new_link:
             logging.warning('Not a valid pornhub link!')
             continue
         else:
-            new_links.append(new_link)
-    return new_links
+            new_data.append(new_link)
+    return new_data
 
-def write_json(data, new_links, filename='./data/data.json'):
+def write_json(downloaded_data, checked_new_data, filename='./data/data.json'):
     with open(filename, 'w', encoding='utf-8') as file:
-        json.dump(({'links':data+new_links}),file)
+        json.dump(({'downloaded_links':downloaded_data, 'new_links':checked_new_data}),file)
 
 def collect_links():
-    data = read_json()
-    new_links = collect(data)
-    write_json(data,new_links)
+    downloaded_data, new_data= get_storage_links()
+    checked_new_data = check(downloaded_data, new_data)
+    write_json(downloaded_data, checked_new_data)
 
 if __name__ == '__main__':
     collect_links()
--- src/downloader.py
+++ src/downloader.py
@@ -3,7 +3,7 @@ import json
 from time import sleep as s
 from twilio.rest import Client
 from logger import pornhub_logger
-def get_links(filename='./data/data.json'):
+def get_links(filename=r'C:\Users\owenw\vscode\projects\Pornhub\data\data.json'):
     with open(filename, 'r') as file:
         return json.load(file)['links']
 
@@ -11,24 +11,15 @@ def get_links(filename='./data/data.json'):
 def download(links):
     options = {
         'noplaylist': True,
-        'format': 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best'
+        'format': 'best[ext=mp4]/best'
     }
     with youtube_dl.YoutubeDL(options) as ydl:
         for link in links:
             try:
                 info_dict = ydl.extract_info(link)
-                video_url = info_dict.get('url', None)
-                video_id = info_dict.get('id', None)
-                video_title = info_dict.get('title', None)
 
             # ydl.download(links)
             except Exception as e:
-                # account_sid = "AC67d1437cea8593339dce6ec20c4eeeb3"
-                # auth_token = "60e2616f01b750c78efd6d44022a8210"
-                # client = Client(account_sid, auth_token)
-                # client = Client(account_sid, auth_token)
-                # message = client.messages.create(
-                #     body={'ph download error'}, from_="+18592953746", to="+62816300111")
                 pornhub_logger.warning(f"{link}\n{e}")
                 continue
 
