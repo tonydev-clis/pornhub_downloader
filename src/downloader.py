8f5f328d0c6dfb1cd5d96eef73727210965f48fb
Owen Valentinus <owenwijaya89@gmail.com>
2023-07-03 11:41:59 +0800

    removed brute force methods for a more elegant solution

--- src/downloader.py
+++ src/downloader.py
@@ -1,40 +1,25 @@
 import youtube_dl
 import json
-from time import sleep as s
-from twilio.rest import Client
-from logger import pornhub_logger
-from collect import get_storage_links
+from concurrent.futures import ThreadPoolExecutor
 
-
-def start_download(new_links):
+def download(link):
     options = {
         'noplaylist': True,
         'format': 'best[ext=mp4]/best',
         'ignoreerrors': True
     }
     with youtube_dl.YoutubeDL(options) as ydl:
-        ydl.download(new_links)
+        ydl.download([link])
 
-def organize_links(downloaded_links, new_links):
-    downloaded_links += new_links
-    return downloaded_links
+def get_links():
+    with open('./data/data.json', 'r') as file:
+        links = json.load(file)['downloaded_links']
+    return links
 
+def start_download(links):
+    with ThreadPoolExecutor(max_workers=5) as executor:
+        executor.map(download, links)
 
-def write_downloaded_json(filename, downloaded_links):
-    with open(filename, 'w') as file:
-        json.dump({
-            'downloaded_links': downloaded_links,
-            'new_links': []
-        },
-                  file)
-
-
-def download(filename='./data/data.json'):
-    downloaded_links, new_links = get_storage_links(filename)
-    start_download(new_links)
-    organized_links = organize_links(downloaded_links, new_links)
-    write_downloaded_json(filename, organized_links)
-
-
-if __name__ == '__main__':
-    download()
+if __name__ == "__main__":
+    links = get_links()
+    start_download(links)
--- src/main.py
+++
@@ -1,3 +0,0 @@
-import requests
-r = requests.get('https://members.hanime.tv/videos/hentai/muttsuri-do-sukebe-2')
-print(r.text)
